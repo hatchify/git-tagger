2c4fd11f6278105658bd90fe66ed5ede15d6f487
Josh Montoya <josh@montoya.io>
2020-03-05 14:22:03 -0800

    Clean up git funcs

--- git.go
+++ git.go
@@ -1,23 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func getCurrentTag() (tag *Tag, err error) {
-	var str string
-	if str, err = runCommand("git", "describe", "--tag"); err != nil {
+	var stdout, stderr string
+	if stdout, stderr, err = runCommand("git", "describe", "--tag"); err != nil {
 		return
 	}
 
-	return newTag(str)
+	if len(stderr) > 0 {
+		err = errors.New(stderr)
+		return
+	}
+
+	return newTag(stdout)
+}
+
+func setTag(tag *Tag) (err error) {
+	var stderr string
+	fmt.Println("Setting tag", tag.String())
+	if _, stderr, err = runCommand("git", "tag", tag.String()); err != nil {
+		return
+	}
+
+	if len(stderr) > 0 {
+		return errors.New(stderr)
+	}
+
+	return
 }
 
 func pushTag(tag *Tag) (err error) {
-	if _, err = runCommand("git", "tag", tag.String()); err != nil {
-		return fmt.Errorf("error setting tag \"%s\":  %v", tag.String(), err)
-	}
-
-	if _, err = runCommand("git", "push", "origin", tag.String()); err != nil {
+	var stdout string
+	// For some reason, git push pipes the output to stderr. Because of this, we set the second
+	// argument as the stdout
+	if _, stdout, err = runCommand("git", "push", "origin", tag.String()); err != nil {
 		return fmt.Errorf("error pushing tag \"%s\" to origin:  %v", tag.String(), err)
 	}
+
+	fmt.Print(stdout)
 	return
 }
